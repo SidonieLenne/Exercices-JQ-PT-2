51cce01cca1e8eff751363cdbc0d3b16a6481018
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-07 15:50:39 +0200

    ça fonctionne mais c'est moche

--- TP1/assets/js/script.js
+++ TP1/assets/js/script.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
     var countClicks = 0;
     $('#button').click(function(){
         if(($('#number').val() < 0) || ($('#number').val() > 100)){
-            alert('On avait dit entre 0 et 100!');
+            $('#moreLess').text('On avait dit entre 0 et 100!');
             countClicks++;
         } else if ($('#number').val() > 38){
-            alert('C\'est moins...');
+            $('#moreLess').text('C\'est moins...');
             countClicks++;
         } else if ($('#number').val() < 38) {
-            alert('C\'est plus...');
+            $('#moreLess').text('C\'est plus...');
             countClicks++;
         }
         else if ($('#number').val() == 38){
             countClicks++;
-            alert('Après '+ countClicks +' essais,\nTu as trouvé!!');
+            $('#moreLess').text('Bravo!\nAprès '+ countClicks +' essais,\nTu as trouvé!!');
         }
         else{
             alert('Erreur');
