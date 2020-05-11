c598c8d662cad6801f7b7748cecdac554e92da8a
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-11 15:22:29 +0200

    TP 2 corrigé

--- TP2/assets/js/script.js
+++ TP2/assets/js/script.js
@@ -1,37 +1,67 @@
 $(document).ready(function(){
     //Fonction déclenchée au clic sur le bouton
     $('#button').click(function(){
-        //Déclaration de mes variables de nombre
-        var number1 = $('#number1').val();
-        var number2 = $('#number2').val();
-        var number3 = $('#number3').val();
-        var number4 = $('#number4').val();
-        var number5 = $('#number5').val();
+        //SI la note est inférieure à 0 OU supérieure à 20, c'est faux, SINON, ça récupète la valeur de l'input
+        //NOTE 1
+        if ((parseInt($('#number1').val(), 10) < 0) || (parseInt($('#number1').val(), 10) > 20)){
+            alert('La note 1 est fausse, triple buse');
+        }
+        else{
+            var number1 = $('#number2').val();
+        }
+        //NOTE 2
+        if ((parseInt($('#number2').val(), 10) < 0) || (parseInt($('#number2').val(), 10) > 20)){
+            alert('La note 2 est fausse, triple buse');
+        }
+        else{
+            var number2 = $('#number2').val();
+        }
+        //NOTE 3
+        if ((parseInt($('#number3').val(), 10) < 0) || (parseInt($('#number3').val(), 10) > 20)){
+            alert('La note 3 est fausse, triple buse');
+        }
+        else{
+            var number3 = $('#number3').val();
+        }
+        //NOTE 4
+        if ((parseInt($('#number4').val(), 10) < 0) || (parseInt($('#number4').val(), 10) > 20)){
+            alert('La note 4 est fausse, triple buse');
+        }
+        else{
+            var number4 = $('#number4').val();
+        }
+        //NOTE 5
+        if ((parseInt($('#number5').val(), 10) < 0) || (parseInt($('#number5').val(), 10) > 20)){
+            alert('La note 5 est fausse, triple buse');
+        }
+        else{
+            var number5 = $('#number5').val();
+        }
         //la moyenne sera tous les nombres additionnés puis divisés par 5
-        somme = (parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4) + parseInt(number5))/5;
+        result = (parseInt(number1, 10) + parseInt(number2, 10) + parseInt(number3, 10) + parseInt(number4, 10) + parseInt(number5, 10))/5;
         /*SI la moyenne est supérieure ou égale à 0 ET inférieure à 10, c'est en dessous de la moyenne
         SINON SI la moyenne est supérieure où égale à 10 ET inférieure à 13, c'est moyen
         SINON SI la moyenne est supérieure où égale à 13 ET inférieure à 16, c'est bien
         SINON SI la moyenne est supérieure où égale à 16 ET inférieure à 20, c'est très bien
         SINON SI la moyenne est égale à 20, c'est excellent
-        SINON, erreur  */
-        if ((somme >= 0) && (somme < 10)){
-            alert('Votre moyenne est de ' + somme + ', c\'est en dessous de la moyenne.');
+        SINON, RECOMMENCE  */
+        if ((result >= 0) && (result < 10)){
+            alert('Ta moyenne est de ' + result + ', c\'est en dessous de la moyenne.');
         } 
-        else if ((somme >= 10) && (somme < 13)){
-            alert('Votre moyenne est de ' + somme + ', c\'est moyen.');
+        else if ((result >= 10) && (result < 13)){
+            alert('Ta moyenne est de ' + result + ', c\'est moyen.');
         } 
-        else if ((somme >= 13) && (somme < 16)){
-            alert('Votre moyenne est de ' + somme + ', c\'est bien.');
+        else if ((result >= 13) && (result < 16)){
+            alert('Ta moyenne est de ' + result + ', c\'est bien.');
         } 
-        else if ((somme >= 16) && (somme < 20)){
-            alert('Votre moyenne est de ' + somme + ', c\'est très bien.');
+        else if ((result >= 16) && (result < 20)){
+            alert('Ta moyenne est de ' + result + ', c\'est très bien.');
         } 
-        else if (somme == 20){
-            alert('Votre moyenne est de ' + somme + ', c\'est excellent.');
+        else if (result == 20){
+            alert('Ta moyenne est de ' + result + ', c\'est excellent.');
         }
         else{
-            alert('Erreur')
+            alert('Recommence')
         }
     });
 });
